6339f9920fdcc35731387e06d0dfdcb0ab894fc7
Natalia <teper.n@gmail.com>
2022-06-22 15:59:17 +0400

    Pagination with system settings

--- app/addons/history/controllers/backend/history.php
+++ app/addons/history/controllers/backend/history.php
@@ -17,7 +17,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 if ($mode === 'order_history') {
     $params = $_REQUEST;
-    $items_per_page = $params['items_per_page'] ? $params['items_per_page'] : 3;
+
+    $items_per_page = $params['items_per_page'] ? $params['items_per_page'] : Registry::get('settings.Appearance.admin_elements_per_page');
+    
     list($changed_orders, $params) = fn_test_get_order_history_data($params, $items_per_page);
     
     Tygh::$app['view']->assign('changed_orders', $changed_orders);   
